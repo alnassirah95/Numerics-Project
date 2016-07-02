d32e19f42d40ef0a44df9944b375b78bb130e032
wshand <willthealmightyone@gmail.com>
2016-07-01 22:56:16 -0600

    Vectorize nth_derivative.m to increase speed for higher step sizes.

--- Methods/Differentiation/nth_derivative.m
+++ Methods/Differentiation/nth_derivative.m
@@ -32,12 +32,6 @@ elseif radius <= 0
     error( 'Radius must be positive.' )
 end
 
-% In order to compute the integral, we store a current offset from the
-% point at which we are trying to compute the integral, which starts off
-% being equal to radius. Then at each iteration we shift z by multiplying
-% it by another variable, angle_shift.
-offset = radius;
-
 % The angle by which we should rotate around the point at which we are
 % trying to evaluate the derivative is equal to 2 * pi / steps. Since we
 % are integrating counterclockwise, we take the conjugate of this value.
@@ -47,43 +41,44 @@ angle_shift = conj( cos(2 * pi / steps) + sin(2 * pi / steps) * 1i );
 % each step. Again, we take the conjugate of this angle.
 half_angle_shift = conj( cos(pi / steps) + sin(pi / steps) * 1i );
 
-% Store the value of the integral on the contour
-integral = 0;
+% We store the offset from a for each step in the following variable. In
+% order to compute the value of z at the beginning of each step, we
+% calculate a + offsets
+offsets = radius * exp( linspace( 0, 2 * pi, steps + 1 ) * 1i );
+
+% We also store the offset from a for the end of each step in the following
+% variable. Again, to calculate the value of z at the end of each step, we
+% calculate a + end_offsets.
+end_offsets = offsets .* angle_shift;
 
 % We store the equation f(z) / (z - a)^(n + 1) in an anonymous function so
-% that we don't have to write the above equation over and over again.
-contour_fn = @(z) f(z) / ( (z - a).^(n + 1) );
+% that we don't have to write the above equation multiple times.
+contour_fn = @(z) f(z) ./ ( (z - a) .^ (n + 1) );
 
-% Now we iterate over the number of steps in order to compute the integral.
-for i = 1:steps
-    % Get the offset from the point at which we are trying to differentiate
-    % that is at the end of the current step.
-    end_offset = offset * angle_shift;
-    % Get the offset from the point at which we are trying to differentiate
-    % that is at the middle of the current step.
-    middle_offset = offset * half_angle_shift;
-    % Use Simpson's rule to approximate the integral between offset and
-    % end_offset. Our "step size" here is equal to (a + end_offset) - (a +
-    % offset) = end_offset - offset, so we multiply by that. We put off
-    % dividing by 6 until the end for increased efficiency.
-    integral = integral + (end_offset - offset) * ...
-        (contour_fn(a + offset) + 4 * contour_fn(a + middle_offset) + ...
-         contour_fn(a + end_offset));
-    % Update the current offset before the next step.
-    offset = end_offset;
-end
+% Now we compute the part of Simpson's rule that is "inside the
+% parentheses" first and store it in a variable.
+integral = contour_fn( offsets + a ) + ...
+            4 * contour_fn( half_angle_shift * offsets + a ) + ...
+            contour_fn( end_offsets + a );
 
+% Now we multiply by step size by subtracting offsets from end_offsets and
+% taking the elementwise product.
+integral = integral .* (offsets - end_offsets);
+
+% Finally, we sum over all the steps and divide by 6 (due to Simpson's 
+% rule) in order to finish computing the integral over the circle.
+integral = sum(integral(:)) / 6;
+        
 % Closures aren't automatically garbage-collected, so we have to clear the
-% closure that we created (contour_fn) from memory.
+% closure that we created (contour_fn) from memory. We also clear offsets
+% and end_offsets from memory since they take up a large amount of space
+% for large numbers of steps.
 clear contour_fn;
-
-% While we were integrating, we didn't divide by 6 in order to increase
-% efficiency; we do so now.
-integral = integral / 6;
+clear offsets;
+clear end_offsets;
 
 % Finally, now that we have computed the contour integral, we multiply by
 % n! / (2 * pi * i) to get the nth derivative (by Cauchy's formula).
 derivative = integral * factorial( n ) / (2 * pi * 1i);
 
 end
-
